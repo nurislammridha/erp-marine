2a266788df93721ae068f99b3b33d138482e141d
intern24.it <intern24.it@akij.net>
2021-01-07 19:16:50 +0600

    responsive complete

--- src/modules/master/components/CircularProgressBar/CircularProgressBar.js
+++ src/modules/master/components/CircularProgressBar/CircularProgressBar.js
@@ -12,47 +12,56 @@ import "react-circular-progressbar/dist/styles.css";
 const CircularProgressBar = () => {
 
     const percentage = 20;
-    return ( 
-      <>
+    return (
+        <>
 
-           <div className="container">
-               <div className="row">
-                   <div className="col-lg-5">
-
+            {/* <div className="container">
+                <div className="row">
+                    <div className="col-lg-5">
                         <div className="action__needed">
-                                <div className="action__head">
-                                    <h4 className="float-left">Action Needed</h4>
-                                      
-                                </div>
+                            <div className="action__head">
+                                <h4 className="float-left">Action Needed</h4>
+                                <h4 className="float-right three__dot">  <i class="fas fa-ellipsis-h"></i></h4>
+                            </div>
 
-                                <div className="action__body">
+                            <div className="action__body">
 
                                 <CircularProgressbar
-                                    
+
                                     className="progress__bar"
                                     value={percentage}
                                     text={`${percentage}%`}
                                     styles={buildStyles({
-                                        textColor: "blue",
-                                        pathColor: "turquoise",
-                                        trailColor: "gold",
+                                        textColor: "black",
+                                        // fontWeight: {900},
+                                        pathColor: "rgba(201, 247, 245, 0.85)",
+                                        trailColor: "rgba(27, 197, 189, 0.85)",
 
                                     })}
-                                />
-                                </div>
 
+                                    strokeWidth={13}
+                                />
+                            </div>
+                          
+                           <div className="action__footer">
+                                <p className="text_center"> Notes: Current sprint requires stakeholders
+                                  to approve newly amended policies</p>
+
+                                <a href="" target="_blank">generate Report</a>
+                           </div>
 
                         </div>
 
-                   </div>
-                   <div className="col-lg-6">
+                    </div>
+                    <div className="col-lg-6">
 
-                   </div>
-               </div>
-           </div>
-          
-      </>
-     );
+                    </div>
+                </div>
+            </div> */}
+           
+
+        </>
+    );
 }
- 
+
 export default CircularProgressBar;
