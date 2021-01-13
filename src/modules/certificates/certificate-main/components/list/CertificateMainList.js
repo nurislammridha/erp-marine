632be35391fab9c414bf3959e34c57397801538f
Intern  IT 36 <intern36.it@akij.net>
2021-01-13 18:05:49 +0600

    main certificate edit

--- src/modules/certificates/certificate-main/components/list/CertificateMainList.js
+++ src/modules/certificates/certificate-main/components/list/CertificateMainList.js
@@ -88,144 +88,144 @@ const CertificateMainList = () => {
     <>
       <Card>
         <Card.Body>
-            <div className="row">
-              <h1 className="tableheading">Certificates</h1>
+          <div className="row">
+            <h1 className="tableheading">Certificates</h1>
 
-              <Form.Group as={Col} controlId="formGridState">
-                <input
-                  type="search"
-                  value={searchText}
-                  className="form-control product-search-input formHeight"
-                  placeholder="Search"
-                  onChange={searchProduct}
-                />
-              </Form.Group>
-              <Form.Group as={Col} controlId="formGridState">
-                <RHFInput
-                  as={<Select options={certificateParentCategoryList} />}
-                  rules={{ required: true }}
-                  name="intCategoryID"
-                  placeholder="Category"
-                  register={register}
-                  value={certificateParentCategoryList.intParentCategoryID}
-                  onChange={(option) => {
-                    certificateSelect(
-                      option.value
-                    );
-                    setValue("intCategoryID", "");
-                    dispatch(getCertificateChildCategoryData(option.value));
-                  }}
-                  setValue={setValue}
-                />
-              </Form.Group>
+            <Form.Group as={Col} controlId="formGridState">
+              <input
+                type="search"
+                value={searchText}
+                className="form-control product-search-input formHeight"
+                placeholder="Search"
+                onChange={searchProduct}
+              />
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridState">
+              <RHFInput
+                as={<Select options={certificateParentCategoryList} />}
+                rules={{ required: true }}
+                name="intCategoryID"
+                placeholder="Category"
+                register={register}
+                value={certificateParentCategoryList.intParentCategoryID}
+                onChange={(option) => {
+                  certificateSelect(
+                    option.value
+                  );
+                  setValue("intCategoryID", "");
+                  dispatch(getCertificateChildCategoryData(option.value));
+                }}
+                setValue={setValue}
+              />
+            </Form.Group>
 
-              <Form.Group as={Col} controlId="formGridState">
-                <RHFInput
-                  as={<Select options={certificateChildCategoryList} />}
-                  rules={{ required: true }}
-                  placeholder="Sub Category"
-                  name="intCategoryID"
-                  register={register}
-                  value={certificateChildCategoryList.intCategoryID}
-                  onChange={(option) => {
-                    certificateSelect(
-                      option.value
-                    );
-                  }}
-                  setValue={setValue}
-                />
-              </Form.Group>
+            <Form.Group as={Col} controlId="formGridState">
+              <RHFInput
+                as={<Select options={certificateChildCategoryList} />}
+                rules={{ required: true }}
+                placeholder="Sub Category"
+                name="intCategoryID"
+                register={register}
+                value={certificateChildCategoryList.intCategoryID}
+                onChange={(option) => {
+                  certificateSelect(
+                    option.value
+                  );
+                }}
+                setValue={setValue}
+              />
+            </Form.Group>
 
-              <Form.Group as={Col} controlId="formGridState">
-                <RHFInput
-                  as={<Select options={certificateExpireDaysList} />}
-                  rules={{ required: true }}
-                  placeholder="Expire In"
-                  name="intCategoryID"
-                  register={register}
-                  value={expireInDays}
-                  onChange={(option) => {
-                    setExpireInDays(
-                      option.value
-                    );
-                    dispatch(getCertificateMainListAction(currentPage, searchText, true, certificateChildCategoryList.intCategoryID, option.value));
-                  }}
-                  setValue={setValue}
-                />
-              </Form.Group>
+            <Form.Group as={Col} controlId="formGridState">
+              <RHFInput
+                as={<Select options={certificateExpireDaysList} />}
+                rules={{ required: true }}
+                placeholder="Expire In"
+                name="intCategoryID"
+                register={register}
+                value={expireInDays}
+                onChange={(option) => {
+                  setExpireInDays(
+                    option.value
+                  );
+                  dispatch(getCertificateMainListAction(currentPage, searchText, true, certificateChildCategoryList.intCategoryID, option.value));
+                }}
+                setValue={setValue}
+              />
+            </Form.Group>
 
-              <i className="fas fa-filter tableFilter mt-3 mr-2"></i>
-              <i className="far fa-filter"></i>
-              <Link to='/certificates-main/create' className="btn btn-primary text-center text-white">
-                Add New
+            <i className="fas fa-filter tableFilter mt-3 mr-2"></i>
+            <i className="far fa-filter"></i>
+            <Link to='/certificates-main/create' className="btn btn-primary text-center text-white">
+              Add New
               </Link>
-            </div>
-            {isLoading && <LoadingSpinner text="Loading Certificates..." />}
-            {
-              !isLoading  && certificates.length > 0 &&
-              <table className="table mt-5 certificate-list tbl-standard table-responsive">
-                <thead>
-                  <tr>
-                    <th className="td-sl">#</th>
-                    {/* <th scope="col">Folder No.</th> */}
-                    <th scope="col">Code</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Type</th>
-                    <th scope="col">Issued By</th>
-                    <th scope="col">Issued Place</th>
-                    <th scope="col">Location</th>
-                    <th scope="col">Issued Date</th>
-                    <th scope="col">Valid Until</th>
-                    <th scope="col">Entended Until</th>
-                    <th scope="col">Last Endorsement</th>
-                    <th scope="col">Not On Board</th>
-                    <th scope="col">Due Date</th>
-                    <th>Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {certificates.map((certificate, index) => (
-                    <tr
-                      className={getCertificateColorClass(
-                        certificate.differenceDays
-                      )}
-                    >
-                      <td>{index + 1}</td>
-                      {/* <td>{certificate.strShipFolderNo}</td> */}
-                      <td>{certificate.strCustomeCode}</td>
-                      <td>{certificate.strShipRemarks}</td>
-                      <td>{certificate.strCertificateTypeName}</td>
-                      <td>{certificate.strIssuingAuthorityName}</td>
-                      <td>{certificate.strIssuedPlace}</td>
-                      <td>{certificate.strLocation}</td>
-                      <td>
-                        {
-                          certificate.dteCertificateIssueDate !== null ? generateStringDateFromDate(
-                            certificate.dteCertificateIssueDate
-                          ) : ''
-                        }
-                      </td>
-                      <td>
-                        {certificate.dteCertificateValidUntil !== null ? generateStringDateFromDate(
-                          certificate.dteCertificateValidUntil
-                        ) : ''}
-                      </td>
-                      <td>
-                        {certificate.dteExtendedUntil !== null ? generateStringDateFromDate(certificate.dteExtendedUntil) : ''}
-                      </td>
-                      <td>
-                        {certificate.dteLastEndorsementDate !== null ? generateStringDateFromDate(
-                          certificate.dteLastEndorsementDate
-                        ) : ''}
-                      </td>
-                      <td>{certificate.intNotOnBoard === "1" ? "Yes" : "No"}</td>
-                      <td>{certificate.differenceDays}</td>
-                      <td className="">
-                        <Link
-                          to={`/certificates-main/edit/${certificate.intCertificateDetailsID}`}
-                        >
-                          <i className="fa fa-edit text-success"></i>
-                        </Link>
+          </div>
+          {isLoading && <LoadingSpinner text="Loading Certificates..." />}
+          {
+            !isLoading && certificates.length > 0 &&
+            <table className="table mt-5 certificate-list tbl-standard table-responsive">
+              <thead>
+                <tr>
+                  <th className="td-sl">#</th>
+                  {/* <th scope="col">Folder No.</th> */}
+                  <th scope="col">Code</th>
+                  <th scope="col">Description</th>
+                  <th scope="col">Type</th>
+                  <th scope="col">Issued By</th>
+                  <th scope="col">Issued Place</th>
+                  <th scope="col">Location</th>
+                  <th scope="col">Issued Date</th>
+                  <th scope="col">Valid Until</th>
+                  <th scope="col">Entended Until</th>
+                  <th scope="col">Last Endorsement</th>
+                  <th scope="col">Not On Board</th>
+                  <th scope="col">Due Date</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {certificates.map((certificate, index) => (
+                  <tr key={index + 1}
+                    className={getCertificateColorClass(
+                      certificate.differenceDays
+                    )}
+                  >
+                    <td>{index + 1}</td>
+                    {/* <td>{certificate.strShipFolderNo}</td> */}
+                    <td>{certificate.strCustomeCode}</td>
+                    <td>{certificate.strShipRemarks}</td>
+                    <td>{certificate.strCertificateTypeName}</td>
+                    <td>{certificate.strIssuingAuthorityName}</td>
+                    <td>{certificate.strIssuedPlace}</td>
+                    <td>{certificate.strLocation}</td>
+                    <td>
+                      {
+                        certificate.dteCertificateIssueDate !== null ? generateStringDateFromDate(
+                          certificate.dteCertificateIssueDate
+                        ) : ''
+                      }
+                    </td>
+                    <td>
+                      {certificate.dteCertificateValidUntil !== null ? generateStringDateFromDate(
+                        certificate.dteCertificateValidUntil
+                      ) : ''}
+                    </td>
+                    <td>
+                      {certificate.dteExtendedUntil !== null ? generateStringDateFromDate(certificate.dteExtendedUntil) : ''}
+                    </td>
+                    <td>
+                      {certificate.dteLastEndorsementDate !== null ? generateStringDateFromDate(
+                        certificate.dteLastEndorsementDate
+                      ) : ''}
+                    </td>
+                    <td>{certificate.intNotOnBoard === "1" ? "Yes" : "No"}</td>
+                    <td>{certificate.differenceDays}</td>
+                    <td className="">
+                      <Link
+                        to={`/certificates-main/edit/${certificate.intCertificateDetailsID}`}
+                      >
+                        <i className="fa fa-edit text-success"></i>
+                      </Link>
                       &nbsp;&nbsp;&nbsp;
                       {/* <button
                         className="btn btn-icon btn-light btn-hover-danger btn-sm"
@@ -240,18 +240,18 @@ const CertificateMainList = () => {
                       >
                         <i className="fa fa-trash"></i>
                       </button> */}
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            }
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          }
 
-            {!isLoading && certificates.length === 0 && (
-              <div className="alert border-1 p-4">
-                Sorry ! No Certificates Found.
-              </div>
-            )}
+          {!isLoading && certificates.length === 0 && (
+            <div className="alert border-1 p-4">
+              Sorry ! No Certificates Found.
+            </div>
+          )}
 
           <PaginationLaravel
             changePage={changePage}
