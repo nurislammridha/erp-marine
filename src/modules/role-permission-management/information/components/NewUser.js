afb5f1ccff1f47a1f47f9dc31a01275c147f7729
Fabliha Afia <afia.corp@akij.net>
2021-01-28 17:43:03 +0600

    new user page

--- src/modules/role-permission-management/information/components/NewUser.js
+++ src/modules/role-permission-management/information/components/NewUser.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Form } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 
 const NewUser = () => {
   return (
@@ -9,63 +9,91 @@ const NewUser = () => {
           <div className="col-12">
             <div className="card card-custom gutter-b p-5">
               <h1>Create New User </h1>
-              <div className="react-bootstrap-table table-responsive">
-                <table className="table table table-head-custom table-vertical-center user-list-table ">
-                  <thead>
-                    <tr>
-                      <th scope="col">SI</th>
-                      <th scope="col">Name</th>
-                      <th scope="col">Email</th>
-                      <th scope="col"></th>
-                      <th scope="col">Roles</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>#01</td>
-                      <td>Super Admin</td>
-                      <td>superadmin@example.com</td>
-                      <td></td>
-                      <td className=" text-white">
-                        <span className="badge badge-primary">superadmin</span>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td>
-                        Action{" "}
-                        <button className="btn btn-success ml-2 btn-sm">
-                          Edit
-                        </button>
-                        <button className="btn btn-danger  btn-sm ml-2">
-                          Delete
-                        </button>
-                      </td>
-                      <td></td>
-                    </tr>
-                    <tr>
-                      <td>#01</td>
-                      <td>Super Admin</td>
-                      <td>superadmin@example.com</td>
-                      <td></td>
-                      <td className=" text-white">
-                        <span className="badge badge-primary">superadmin</span>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>#01</td>
-                      <td>Super Admin</td>
-                      <td>superadmin@example.com</td>
-                      <td></td>
-                      <td className=" text-white">
-                        <span className="badge badge-primary">superadmin</span>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
+              <form
+                className="form form-label-right voyageEngineerForm"
+                method="post"
+              >
+                <div className="form-group row">
+                  <div className="col-xl-3 col-lg-3 col-md-6 ">
+                    <Form.Group>
+                      <Form.Label className="formFont pl-1">
+                        Admin Name
+                      </Form.Label>
+                      <Form.Control
+                        className="formHeight"
+                        type="text"
+                        placeholder="Enter Name"
+                      />
+                    </Form.Group>
+                  </div>
+                  <div className="col-xl-3 col-lg-3 col-md-6 ">
+                    <Form.Group>
+                      <Form.Label className="formFont pl-1">
+                        Admin Email
+                      </Form.Label>
+                      <Form.Control
+                        className="formHeight"
+                        type="text"
+                        placeholder="Enter email"
+                      />
+                    </Form.Group>
+                  </div>
+
+                  <div className="col-xl-3 col-lg-3 col-md-6 ">
+                    <Form.Group>
+                      <Form.Label className="formFont pl-1">
+                        Password
+                      </Form.Label>
+                      <Form.Control
+                        className="formHeight"
+                        type="text"
+                        placeholder="Enter Username"
+                      />
+                    </Form.Group>
+                  </div>
+                  <div className="col-xl-3 col-lg-3 col-md-6">
+                    <Form.Group>
+                      <Form.Label className="formFont pl-1">
+                        Confirm Password
+                      </Form.Label>
+                      <Form.Control
+                        className="formHeight"
+                        type="text"
+                        placeholder="Enter Password"
+                      />
+                    </Form.Group>
+                  </div>
+                </div>
+                <div className="form-group row">
+                  <div className="col-xl-3 col-lg-3 col-md-6 ">
+                    <Form.Group>
+                      <Form.Label className="formFont pl-1">
+                        Assign Roles
+                      </Form.Label>
+                      <Form.Control
+                        className="formHeight"
+                        type="text"
+                        placeholder="Type"
+                      />
+                    </Form.Group>
+                  </div>
+                  <div className="col-xl-3 col-lg-3 col-md-6 ">
+                    <Form.Group>
+                      <Form.Label className="formFont pl-1">
+                        Admin Username
+                      </Form.Label>
+                      <Form.Control
+                        className="formHeight"
+                        type="text"
+                        placeholder="Enter Username"
+                      />
+                    </Form.Group>
+                  </div>
+                </div>
+                <Button className="mr-4  saveButton text-white" variant="">
+                  Submit
+                </Button>
+              </form>
             </div>
           </div>
         </div>
--- src/modules/role-permission-management/information/components/UserModal.js
+++ src/modules/role-permission-management/information/components/UserModal.js
@@ -1,49 +1,23 @@
 import React, { useState } from "react";
 import { Modal } from "react-bootstrap";
 import { Form, Card, Button } from "react-bootstrap";
-// import Select from "react-select";
-// import makeAnimated from "react-select/animated";
-// import { colourOptions } from "../data";
+import SimpleModal from "../../../master/components/Modal/SimpleModal";
 
-// const animatedComponents = makeAnimated();
-const UserModal = () => {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+const UserModal = ({ show, setShow }) => {
   return (
     <div>
-      <Button variant="primary" onClick={handleShow} className="btn-sm">
-        Assign Roles
-      </Button>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Body>
-          {/* <Select
-            closeMenuOnSelect={false}
-            components={animatedComponents}
-            defaultValue={[colourOptions[4], colourOptions[5]]}
-            isMulti
-            options={colourOptions}
-          /> */}
-          <Form.Group>
-            <Form.Label className="formFont pl-1">Input</Form.Label>
-            <Form.Control
-              className="formHeight"
-              type="text"
-              placeholder="Type"
-            />
-          </Form.Group>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            submit
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      <SimpleModal
+        show={show}
+        handleClose={() => setShow(false)}
+        handleShow={() => setShow(true)}
+        modalTitle={"Assign Roles"}
+      >
+        <Form.Group>
+          <Form.Label className="formFont pl-1"> Assign Roles</Form.Label>
+          <Form.Control className="formHeight" type="text" placeholder="Type" />
+        </Form.Group>
+        <Button variant="primary">Submit</Button>
+      </SimpleModal>
     </div>
   );
 };
