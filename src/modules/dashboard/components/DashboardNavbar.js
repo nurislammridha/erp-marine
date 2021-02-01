8b454a949107f64e8e11dffe5299cc5e915ac708
Fabliha Afia <afia.corp@akij.net>
2021-02-01 13:05:03 +0600

    dashboard

--- src/modules/dashboard/components/DashboardNavbar.js
+++ src/modules/dashboard/components/DashboardNavbar.js
@@ -12,17 +12,26 @@ const DashboardNavbar = () => {
   
 
     <div class="d-flex align-items-center flex-wrap mr-1 dashboard-navbar-left">
-    <div class="d-flex align-items-baseline mr-5"><h5 class="text-dark font-weight-bold my-2 mr-5 ml-5">Dashboard</h5></div>
-    <ul class="breadcrumb breadcrumb-transparent breadcrumb-dot font-weight-bold p-0 my-2  dashboard-nav">
+    <div class="d-flex align-items-baseline mr-5"><h5 class="text-muted font-weight-bold my-2 mr-5 ml-5">Welcome to iMarine, <span className="text-dark font-weight-bold">Mr.Kabir</span></h5></div>
+    {/* <ul class="breadcrumb breadcrumb-transparent breadcrumb-dot font-weight-bold p-0 my-2  dashboard-nav">
         <li class="breadcrumb-item">
             <a href="/metronic/react/demo1/dashboard"><i class="flaticon2-shelter text-muted icon-1x"></i></a>
         </li>
         <li class="breadcrumb-item"><a class="text-muted" href="/metronic/react/demo1/dashboard">Dashboard</a></li>
-    </ul>
+    </ul> */}
 </div>
 <div class="d-flex  float-right dashboard-navbar-right">
     <a href="#" class="btn btn-light btn-sm font-weight-bold" id="kt_dashboard_daterangepicker" data-toggle="tooltip" title="Select dashboard daterange" data-placement="left">
-        <span class="text-muted font-weight-bold mr-2" id="kt_dashboard_daterangepicker_title">Today</span><span class="text-primary font-weight-bold" id="kt_dashboard_daterangepicker_date">Aug 16</span>
+        <span class="text-primary font-weight-bold " id="kt_dashboard_daterangepicker_title">Today</span>
+    </a>
+    <a href="#" class="btn  btn-sm font-weight-bold" id="kt_dashboard_daterangepicker" data-toggle="tooltip" title="Select dashboard daterange" data-placement="left">
+        <span class="text-muted font-weight-bold " id="kt_dashboard_daterangepicker_title">This Week</span>
+    </a>
+    <a href="#" class="btn  btn-sm font-weight-bold" id="kt_dashboard_daterangepicker" data-toggle="tooltip" title="Select dashboard daterange" data-placement="left">
+        <span class="text-muted font-weight-bold" id="kt_dashboard_daterangepicker_title">This Month</span>
+    </a>
+    <a href="#" class="btn  btn-sm font-weight-bold" id="kt_dashboard_daterangepicker" data-toggle="tooltip" title="Select dashboard daterange" data-placement="left">
+        <span class="text-muted font-weight-bold" id="kt_dashboard_daterangepicker_title">This Financial Year</span>
     </a>
    
 </div>
