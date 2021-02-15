d31896e8aede5c992f55982574b8d98ccafa3e5d
Intern  IT 35 <intern35.it@akij.net>
2021-02-15 17:06:11 +0600

    supplier list design done

--- src/modules/partners/suppliers-list/views/SuppliersListContainer.js
+++ src/modules/partners/suppliers-list/views/SuppliersListContainer.js
@@ -15,68 +15,78 @@ const SuppliersListContainer = () => {
     <Card>
       <Card.Body>
         <div className="container">
-          <div className="row">
-            <div className="col-lg-2">
-              <h1 className="tableheading">Suppliers List</h1>
-            </div>
+          <div className="col-lg-2">
+            <h1 className="tableheading">Suppliers List</h1>
+          </div>
+          <div className="row mt-5">
+
+
             <SuppliersFilter />
-            <div className="col-lg-2 export">
-              <Dropdown className="d-inline">
-                <Dropdown.Toggle
-                  className="btn btn-sm"
-                  variant="light text-primary"
-                  id="dropdown-basic"
-                >
-                  Export
+
+
+
+
+            <div className="col-md-4 export">
+              <div className="float-right mr-10">
+
+
+                <Dropdown className="d-inline">
+                  <Dropdown.Toggle
+                    className="btn btn-sm"
+                    variant="light text-primary"
+                    id="dropdown-basic"
+                  >
+                    Export
                 </Dropdown.Toggle>
 
-                <Dropdown.Menu>
-                  <Pdf
-                    targetRef={ref}
-                    filename="SupplierList.pdf"
-                    x={0.5}
-                    y={0.5}
-                    scale={0.93}
-                  >
-                    {({ toPdf }) => (
-                      <Dropdown.Item onClick={toPdf}>
-                        <i class="far fa-file-pdf"></i>
-                        <span className="ml-3">Pdf</span>
-                      </Dropdown.Item>
-                    )}
-                  </Pdf>
+                  <Dropdown.Menu>
+                    <Pdf
+                      targetRef={ref}
+                      filename="SupplierList.pdf"
+                      x={0.5}
+                      y={0.5}
+                      scale={0.93}
+                    >
+                      {({ toPdf }) => (
+                        <Dropdown.Item onClick={toPdf}>
+                          <i class="far fa-file-pdf"></i>
+                          <span className="ml-3">Pdf</span>
+                        </Dropdown.Item>
+                      )}
+                    </Pdf>
 
-                  <Dropdown.Item>
-                    <i class="far fa-file-excel mt-1"></i>
-                    {
-                      <ReactHTMLTableToExcel
-                        className="excelBtn ml-1"
-                        table="supplier-excel"
-                        filename="SupplierList"
-                        sheet="tablexls"
-                        buttonText="Excel"
-                      />
-                    }
-                  </Dropdown.Item>
+                    <Dropdown.Item>
+                      <i class="far fa-file-excel mt-1"></i>
+                      {
+                        <ReactHTMLTableToExcel
+                          className="excelBtn ml-1"
+                          table="supplier-excel"
+                          filename="SupplierList"
+                          sheet="tablexls"
+                          buttonText="Excel"
+                        />
+                      }
+                    </Dropdown.Item>
 
-                  <ReactToPrint
-                    trigger={() => (
-                      <Dropdown.Item href="#/action-1" onClick={window.print}>
-                        <i class="fas fa-print"></i>
-                        <span className="ml-2">Print</span>
-                      </Dropdown.Item>
-                    )}
-                    content={() => ref.current}
-                  />
-                </Dropdown.Menu>
-              </Dropdown>
-              <Button
-                className="btn btn-sm ml-5"
-                variant="primary"
-                onClick={() => history.push("/partners/info")}
-              >
-                Add New
+                    <ReactToPrint
+                      trigger={() => (
+                        <Dropdown.Item href="#/action-1" onClick={window.print}>
+                          <i class="fas fa-print"></i>
+                          <span className="ml-2">Print</span>
+                        </Dropdown.Item>
+                      )}
+                      content={() => ref.current}
+                    />
+                  </Dropdown.Menu>
+                </Dropdown>
+                <Button
+                  className="btn btn-sm ml-5"
+                  variant="primary"
+                  onClick={() => history.push("/partners/info")}
+                >
+                  Add New
               </Button>
+              </div>
             </div>
           </div>
           <div id="id" ref={ref} className="mt-5">
